148bee49cdfd8963bc81b39ebd0d014142692a02
Lawrence Siden <lsiden@gmail.com>
2018-03-20 23:37:26 -0400

    Restores FilmDetail to pure component

--- src/film-detail.js
+++ src/film-detail.js
@@ -9,8 +9,8 @@ import { viewList } from "./actions"
 import { detailStyles } from "./styles"
 import { LEFT_TRIANGLE } from "./constants"
 
-function renderItem(item) {
-  const { text, cond = true, url } = item
+function renderDetail(detail) {
+  const { text, cond = true, url } = detail
 
   if (!cond) {
     return null
@@ -32,73 +32,62 @@ function renderItem(item) {
   return <ListItem {...props} />
 }
 
-const keyExtractor = item => cuid.slug()
+const imdbUrl = imdbID => `https://www.imdb.com/title/${imdbID}`
 
-export class filmDetail extends React.Component {
-  constructor(props) {
-    const { imdbID } = props.filmDetails
-    super(props)
-    this.imdbUrl = `https://www.imdb.com/title/${imdbID}`
-    this.dispatchViewList = props.dispatchViewList.bind(this)
-  }
+const renderBackButton = ({ dispatchViewList }) => (
+  <TouchableOpacity onPress={dispatchViewList}>
+    <Text style={detailStyles.buttonText}>{LEFT_TRIANGLE}</Text>
+  </TouchableOpacity>
+)
 
-  renderBackButton() {
-    const { dispatchViewList } = this.props
-    return (
-      <TouchableOpacity onPress={dispatchViewList}>
-        <Text style={detailStyles.buttonText}>{LEFT_TRIANGLE}</Text>
-      </TouchableOpacity>
-    )
-  }
+const renderHeader = props => {
+  const { filmSummary } = props
+  return (
+    <View style={detailStyles.header}>
+      {renderBackButton(props)}
+      <Text style={detailStyles.title}>{filmSummary.Title}</Text>
+    </View>
+  )
+}
 
-  renderHeader() {
-    const { filmSummary } = this.props
-    return (
-      <View style={detailStyles.header}>
-        {this.renderBackButton()}
-        <Text style={detailStyles.title}>{filmSummary.Title}</Text>
-      </View>
-    )
-  }
+const getDetails = (filmDetails, filmSummary) => [
+  { text: filmSummary.Year },
+  { text: `Directed by ${filmDetails.Director}` },
+  { text: `Written by ${filmDetails.Writer}` },
+  { text: `Cast: ${filmDetails.Actors}` },
+  { text: `Language: ${filmDetails.Language}` },
+  { text: `Awards: ${filmDetails.Awards}`, cond: !!filmDetails.Awards },
+  { text: `Run Time: ${filmDetails.Runtime}` },
+  { text: `IMDB Rating: ${filmDetails.imdbRating}/10` },
+  { text: `Box Office: ${filmDetails.BoxOffice}` },
+  {
+    text: "Official Website",
+    url: filmDetails.Website,
+    cond: filmDetails.Website && filmDetails.Website !== "N/A",
+  },
+  {
+    text: "IMDB page",
+    url: filmDetails.imdbID ? imdbUrl(filmDetails.imdbID) : null,
+  },
+]
 
-  getDetails() {
-    const { filmSummary, filmDetails } = this.props
-    return [
-      { text: filmSummary.Year },
-      { text: `Directed by ${filmDetails.Director}` },
-      { text: `Written by ${filmDetails.Writer}` },
-      { text: `Cast: ${filmDetails.Actors}` },
-      { text: `Language: ${filmDetails.Language}` },
-      { text: `Awards: ${filmDetails.Awards}`, cond: !!filmDetails.Awards },
-      { text: `Run Time: ${filmDetails.Runtime}` },
-      { text: `IMDB Rating: ${filmDetails.imdbRating}/10` },
-      { text: `Box Office: ${filmDetails.BoxOffice}` },
-      {
-        text: "Official Website",
-        url: filmDetails.Website,
-        cond: filmDetails.Website && filmDetails.Website !== "N/A",
-      },
-      {
-        text: "IMDB page",
-        url: this.imdbUrl ? this.imdbUrl : null,
-      },
-    ]
-  }
-
-  render() {
-    const { filmSummary, filmDetails } = this.props
-    return (
-      <View>
-        {this.renderHeader()}
-        <Image
-          source={{ uri: filmSummary.Poster }}
-          style={detailStyles.poster}
-          resizeMode={"contain"}
-        />
-        <List>{this.getDetails().map((item, i) => renderItem(item))}</List>
-      </View>
-    )
-  }
+export const filmDetail = props => {
+  const { filmSummary, filmDetails } = props
+  return (
+    <View>
+      {renderHeader(props)}
+      <Image
+        source={{ uri: filmSummary.Poster }}
+        style={detailStyles.poster}
+        resizeMode={"contain"}
+      />
+      <List>
+        {getDetails(filmDetails, filmSummary).map(detail =>
+          renderDetail(detail)
+        )}
+      </List>
+    </View>
+  )
 }
 
 filmDetail.propTypes = {
