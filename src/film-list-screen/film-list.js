96f6bca5db84dfb08efeea51c85d3f3865e2da67
Lawrence Siden <lsiden@gmail.com>
2018-03-31 22:37:03 -0400

    Refactors film-list

--- src/film-list-screen/film-list.js
+++ src/film-list-screen/film-list.js
@@ -1,44 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
-import { FlatList } from "react-native"
-import { List } from "react-native-elements"
+import { FlatList, TouchableOpacity } from "react-native"
+import { List, ListItem } from "react-native-elements"
 import cuid from "cuid"
 
+import { updateFilmDetails } from "actions"
 import { fetchQueryResultPage, fetchFilmDetails } from "actions/fetch"
-import FilmListItem from "./film-list-item"
 import Header from "./header"
 import Footer from "./footer"
-import { updateFilmDetails } from "actions"
-
-/*
-TODO - Seeing console message:
-13:07:56: VirtualizedList: You have a large list that is slow to update - make sure your renderItem function renders components that follow React performance best practices like PureComponent, shouldComponentUpdate, etc. Object {
-13:07:56:   "contentLength": 2946,
-13:07:56:   "dt": 2493,
-13:07:56:   "prevDt": 7814,
-13:07:56: }
-*/
 
 export const filmList = ({
   navigation,
   films,
   totalResults,
   dispatchFetchPage,
-  dispatchFetchFilmDetails,
-  dispatchResetFilmDetails
+  dispatchFetchFilmDetails
 }) => {
   const renderFilmListItem = props => {
     const filmSummary = props.item
+    const onPress = () => {
+      dispatchFetchFilmDetails(filmSummary.imdbID)
+      navigation.navigate("FilmDetails", { filmSummary })
+    }
     return (
-      <FilmListItem
-        filmSummary={filmSummary}
-        onPress={() => {
-          dispatchResetFilmDetails()
-          dispatchFetchFilmDetails(filmSummary.imdbID)
-          navigation.navigate("FilmDetails", { filmSummary })
-        }}
-      />
+      <TouchableOpacity onPress={onPress}>
+        <ListItem title={filmSummary.Title} subtitle={filmSummary.Year} />
+      </TouchableOpacity>
     )
   }
   renderFilmListItem.propTypes = {
@@ -65,8 +53,7 @@ filmList.propTypes = {
   films: PropTypes.arrayOf(PropTypes.object),
   totalResults: PropTypes.number,
   dispatchFetchPage: PropTypes.func.isRequired,
-  dispatchFetchFilmDetails: PropTypes.func.isRequired,
-  dispatchResetFilmDetails: PropTypes.func.isRequired
+  dispatchFetchFilmDetails: PropTypes.func.isRequired
 }
 filmList.defaultProps = {
   isFetching: false
@@ -79,7 +66,9 @@ export default connect(
   }),
   dispatch => ({
     dispatchFetchPage: () => dispatch(fetchQueryResultPage()),
-    dispatchFetchFilmDetails: imdbID => dispatch(fetchFilmDetails(imdbID)),
-    dispatchResetFilmDetails: () => dispatch(updateFilmDetails(null))
+    dispatchFetchFilmDetails: imdbID => {
+      dispatch(updateFilmDetails(null))
+      dispatch(fetchFilmDetails(imdbID))
+    }
   })
 )(filmList)
