7b485e75c89ae6c5178d0397a70cf269bfe7d7e5
Lawrence Siden <lsiden@gmail.com>
2018-04-05 10:36:57 -0400

    Renames omdb-viewer as app

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Provider } from "react-redux"
 
-import OmdbViewer from "omdb-viewer"
+import MainApp from "app"
 import { getStore } from "store"
 
 export const app = () => (
   <Provider store={getStore()}>
-    <OmdbViewer />
+    <MainApp />
   </Provider>
 )
 
