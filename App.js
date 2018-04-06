1b310eed96c65b6bb082c3ea6df8eb2e92af236c
Lawrence Siden <lsiden@gmail.com>
2018-04-06 15:33:48 -0400

    Ignores "Remote Debugger" warning

--- App.js
+++ App.js
@@ -4,10 +4,14 @@ import { Provider } from "react-redux"
 import MainApp from "app"
 import { getStore } from "store"
 
-export const app = () => (
-  <Provider store={getStore()}>
-    <MainApp />
-  </Provider>
-)
+export const app = () => {
+  // eslint-disable-next-line no-console
+  console.ignoredYellowBox = ["Remote debugger"]
+  return (
+    <Provider store={getStore()}>
+      <MainApp />
+    </Provider>
+  )
+}
 
 export default app
